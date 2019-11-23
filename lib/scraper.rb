1f0c3a04e4eae9158f34cb45f9131bd9da1f6b06
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-23 01:35:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,10 @@ require 'colorize'
 html = open("https://www.wikipedia.org/")
 doc = Nokogiri::HTML(html)
 
-languages =  doc.css(".central-featured-lang").colorize
+languages =  doc.css(".central-featured-lang")
 
 languages.each do |lang|
   puts lang.text.strip
 end
+
+"jeff".colorize(:red)
