eb530bc4ba5ec4824d5824579b43cc8822815b5c
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-23 01:32:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://www.wikipedia.org/")
 doc = Nokogiri::HTML(html)
 
-languages =  doc.css(".central-featured-lang")
+languages =  doc.css(".central-featured-lang").colorize
 
 languages.each do |lang|
   puts lang.text.strip
