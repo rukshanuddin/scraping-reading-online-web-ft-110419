501e322368d7905b3934ef309a12da28be10b33c
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-23 01:30:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://www.wikipedia.org/")
+doc = Nokogiri::HTML(html)
+
+languages =  doc.css(".central-featured-lang")
+
+languages.each do |lang|
+  puts lang.text.strip
+end
